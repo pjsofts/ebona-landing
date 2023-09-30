7d91466e7d0b31a20cbbd4e65a5036ae42ec49a9
Pouria Jahandideh <pjsofts@gmail.com>
2023-10-01 00:17:38 +0330

    responsivity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,10 +10,10 @@ export default function Home() {
   return (
     <div>
       <div className="bg-[#262626] h-[55px]"></div>
-      <div className="container mx-auto sm:mt-14 flex justify-between items-center]">
+      <div className="sm:mt-14 flex justify-between items-center">
         <div className="flex">
           <Image
-            className="m-2 sm:mr-4"
+            className="m-4 sm:mr-4"
             alt="bona"
             src="/images/bona.svg"
             width={74}
@@ -21,16 +21,16 @@ export default function Home() {
             sizes="(max-width: 768px) 40px, (max-width: 1200px) 74px"
             priority
           />
-          <div className="text-neutral-800 text-xl sm:text-4xl font-medium flex items-center">
+          <div className="text-neutral-800 text-xl sm:text-2xl lg:text-4xl font-medium flex items-center">
             Ebona AI
           </div>
         </div>
         <div className="hidden sm:flex sm:space-x-5 sm:items-center" ref={menu}>
-          <div className="sm:text-xl">Home</div>
-          <div className="sm:text-xl">About Us</div>
-          <div className="sm:text-xl">Services</div>
-          <div className="sm:text-xl">Our Features</div>
-          <div className="sm:text-xl">Contact Us</div>
+          <div className="sm:text-lg lg:text-xl">Home</div>
+          <div className="sm:text-lg lg:text-xl">About Us</div>
+          <div className="sm:text-lg lg:text-xl">Services</div>
+          <div className="sm:text-lg lg:text-xl">Our Features</div>
+          <div className="sm:text-lg lg:text-xl">Contact Us</div>
         </div>
 
         <div className="sm:hidden flex items-center">
@@ -46,10 +46,10 @@ export default function Home() {
           </button>
         </div>
 
-        <div className="flex items-center justify-center mr-2">
+        <div className="flex items-center justify-center mr-4">
           <button
             type="button"
-            className="w-20 h-10 rounded-lg sm:w-48 sm:h-20 sm:rounded-[18px] sm:text-3xl font-medium bg-[#FF8F00] text-white"
+            className="w-20 h-10 rounded-lg sm:w-30 lg:w-48 sm:h-15 lg:h-20 sm:rounded-[18px] sm:text-lg lg:text-3xl font-medium bg-[#FF8F00] text-white"
           >
             Call Us
           </button>
@@ -59,14 +59,14 @@ export default function Home() {
       <div className="container sm:flex sm:mt-[120px]">
         <div className="font-medium grow justify-center flex ml-2">
           <div>
-            <div className="text-4xl sm:text-[95px] sm:leading-[95px]">
+            <div className="text-6xl sm:text-[95px] sm:leading-[95px]">
               EBONA <br />
               Education
             </div>
-            <div className="text-2xl sm:text-[33px] mt-4 sm:mt-4">
+            <div className="text-4xl sm:text-[33px] mt-4 sm:mt-4">
               Your Ai Powered
             </div>
-            <div className="text-2xl sm:text-[33px] sm:mt-2">
+            <div className="text-4xl sm:text-[33px] sm:mt-2">
               Education Mentor
             </div>
           </div>
